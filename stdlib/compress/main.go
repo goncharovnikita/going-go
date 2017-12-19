5cf2c7e410547490c72170c3e07a4cb7cfcc9fe7
goncharovnikita <goncharovnikita.dev@gmail.com>
2017-12-19 10:17:37 +0300

    Create gzip compress function

--- stdlib/compress/main.go
+++ stdlib/compress/main.go
@@ -2,13 +2,16 @@ package main
 
 import "bytes"
 import "compress/gzip"
+import "os"
 
 func main() {}
 
 // CompressGzip compress bytes to gzip
-func CompressGzip(file []byte) (result []byte, err error) {
+func CompressGzip(filename string, file []byte) (result []byte, err error) {
 	var buf = new(bytes.Buffer)
 	var gw = gzip.NewWriter(buf)
+
+	gw.Name = filename
 
 	if _, err = gw.Write(file); err != nil {
 		return nil, err
@@ -18,5 +21,37 @@ func CompressGzip(file []byte) (result []byte, err error) {
 		return nil, err
 	}
 
-	return
+	return buf.Bytes(), err
 }
+
+// Gzipify compress given data to .gz file
+func Gzipify(filename string, data []byte) (err error) {
+	var compressed []byte
+	var file *os.File
+	var gzFilename = filename + ".gz"
+
+	if compressed, err = CompressGzip(filename, data); err != nil {
+		return err
+	}
+
+	if file, err = os.OpenFile(gzFilename, os.O_RDWR|os.O_CREATE, 0644); err != nil {
+		return err
+	}
+
+	if _, err = file.Write(compressed); err != nil {
+		return err
+	}
+
+	if err = file.Close(); err != nil {
+		return err
+	}
+
+	return err
+}
+
+// Gunzip ungzip compressed file
+// func Gunzip(filename string) (result []byte, err error)  {
+// 	var (
+// 		gr *gzip.Reader
+// 	)
+// }
--- stdlib/compress/main_test.go
+++ stdlib/compress/main_test.go
@@ -3,25 +3,33 @@ package main_test
 import "testing"
 import "bytes"
 import "compress/gzip"
-import "io"
+
 import "github.com/stretchr/testify/assert"
 import compress "github.com/goncharovnikita/going-go/stdlib/compress"
+import "os"
 
 func TestCompressGzip(t *testing.T) {
 	var (
 		testData = "some test string"
+		filename = "testFilename"
 		result   []byte
 		err      error
 		buf      = new(bytes.Buffer)
 		gr       *gzip.Reader
 	)
 
-	if result, err = compress.CompressGzip([]byte(testData)); err != nil {
+	if result, err = compress.CompressGzip(filename, []byte(testData)); err != nil {
 		t.Fatal(err)
 	}
 
+	t.Log(result)
+
 	reader := bytes.NewReader(result)
-	if gr, err = gzip.NewReader(reader); err != io.EOF && err != nil {
+	if gr, err = gzip.NewReader(reader); err != nil {
+		t.Fatal(err)
+	}
+
+	if err = gr.Close(); err != nil {
 		t.Fatal(err)
 	}
 
@@ -30,4 +38,47 @@ func TestCompressGzip(t *testing.T) {
 	}
 
 	assert.Equal(t, testData, string(buf.Bytes()))
+}
+
+func TestGzipify(t *testing.T) {
+	var (
+		filename   = "filename"
+		gzFilename = "filename.gz"
+		data       = "some test data"
+		buf        = new(bytes.Buffer)
+		b          = new(bytes.Buffer)
+		file       *os.File
+		gr         *gzip.Reader
+		err        error
+	)
+
+	if err = compress.Gzipify(filename, []byte(data)); err != nil {
+		t.Fatal(err)
+	}
+
+	if file, err = os.OpenFile(gzFilename, os.O_RDONLY, 0644); err != nil {
+		t.Fatal(err)
+	}
+
+	if _, err = buf.ReadFrom(file); err != nil {
+		t.Fatal(err)
+	}
+
+	if err = file.Close(); err != nil {
+		t.Fatal(err)
+	}
+
+	reader := bytes.NewReader(buf.Bytes())
+	if gr, err = gzip.NewReader(reader); err != nil {
+		t.Fatal(err)
+	}
+
+	if _, err = b.ReadFrom(gr); err != nil {
+		t.Fatal(err)
+	}
+
+	assert.Equal(t, data, string(b.Bytes()))
+
+	os.Remove(gzFilename)
+
 }
