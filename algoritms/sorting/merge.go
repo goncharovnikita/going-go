a0808ae3cd286ae51b591ae184eca479449ef85a
Unknown <goncharovnikita.dev@gmail.com>
2017-12-24 18:37:24 +0300

    Fix merge sort

--- algoritms/sorting/merge.go
+++ algoritms/sorting/merge.go
@@ -6,10 +6,9 @@ import (
 
 // Merge sort
 func Merge(arr []int) (result []int) {
-	if len(arr) > 1 {
-		q := len(arr) / 2
-		return merge.Merge(Merge(arr[0:q]), Merge(arr[q:]))
+	if len(arr) < 2 {
+		return arr
 	}
-	result = arr
-	return
+	q := len(arr) / 2
+	return merge.Merge(Merge(arr[0:q]), Merge(arr[q:]))
 }
